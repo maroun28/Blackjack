82833cf14b0de408b6b85dcda59a1b0e8d5f5bd7
maroun <marounhdjrjrj@gmail.com>
2026-03-25 11:01:36 +0200

    random shuffle of 52 cards

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
+import random
+cards = []
 suits = ["spades", "clubs", "hearts", "diamonds"]
-suit = suits[2]
-rank = "k"
-value = 10
-# print(f"Your card is : {rank} of {suit}" )
-print("Your card is : " + rank + " of " + suit)
-suits.append("snakes")
-# suits += ["snakes"]
+ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
 for suit in suits:
-    print(suit)
+    for rank in ranks:
+        cards.append([suits, ranks])
+
+random.shuffle(cards)
+print(cards)
