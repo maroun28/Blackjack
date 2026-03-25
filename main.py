b40e3d9c3b539767e29a3a915f8e104955e07497
maroun <marounhdjrjrj@gmail.com>
2026-03-25 11:30:43 +0200

    deal initial setup

--- main.py
+++ main.py
@@ -4,7 +4,20 @@ suits = ["spades", "clubs", "hearts", "diamonds"]
 ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
 for suit in suits:
     for rank in ranks:
-        cards.append([suits, ranks])
+        cards.append([suit, rank])
 
-random.shuffle(cards)
-print(cards)
+def shuffle():
+    random.shuffle(cards)
+
+def deal(number):
+    cards_dealt = []
+    for x in range(number):
+        card = cards.pop()
+        cards_dealt.append(card)
+    return cards_dealt
+
+shuffle()
+cards_dealt = deal(2)
+card = cards_dealt[0]
+print (card)
+
